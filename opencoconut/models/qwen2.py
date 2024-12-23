attempt to fix attention mask (still off by 1)

opencoconut/models/qwen2.py
@@ -283,12 +283,17 @@ class CoconutQwen2ForCausalLM(Qwen2ForCausalLM):
                     all_thought_outputs.append(self.hidden_states_to_token(current_hidden, lm_head=True))
 
             inputs_embeds = self.get_input_embeddings()(language_ids)
+            masked_question_and_thoughts = torch.zeros(
+                (language_ids.shape[0], thought_ids.shape[1]),
+                dtype=language_mask.dtype,
+                device=language_mask.device,
+            )
+            attention_mask = torch.cat((thought_mask, masked_question_and_thoughts, language_mask), dim=1)
             cache_position = torch.arange(
                 cache_position[-1].item(), cache_position[-1].item() + language_ids.shape[1],
                 dtype=cache_position.dtype,
                 device=cache_position.device,
             )
-            attention_mask = torch.cat((torch.ones((language_ids.shape[0], cache_position[-1].item()), device=language_ids.device), language_mask), dim=1)
 
             # Forward pass with combined embeddings
             outputs = super().forward(
@@ -307,12 +312,18 @@ class CoconutQwen2ForCausalLM(Qwen2ForCausalLM):
             )
 
             if self.debug:
+                # FIXME: we are off by 1, but 2 thoughts are generated
+                # print(input_ids.shape, attention_mask.shape)
+                # print(" ".join(
+                #     f"<{self.tokenizer.decode(id)}> ({mask})"
+                #     for id, mask in zip(input_ids[0].tolist(), attention_mask[0].tolist())
+                # ))
                 self._print_thought_and_final_tokens(outputs.logits, all_thought_outputs)
         else:
             # Standard forward pass
             outputs = super().forward(
                 input_ids=input_ids,
-                attention_mask=attention_mask,
+                attention_mask=language_mask,
                 position_ids=position_ids,
                 past_key_values=past_key_values,
                 inputs_embeds=None,